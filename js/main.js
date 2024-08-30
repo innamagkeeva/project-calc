33f1f2115662e24e1aae2fdedd8260be7312f08b
innamagkeeva <inna.m79@mail.ru>
2024-08-30 03:12:36 +0300

    deleteLastDigit

--- js/main.js
+++ js/main.js
@@ -11,29 +11,30 @@ const UI = {
     // NINE: document.querySelector('.button-9'),
     // ZERO: document.querySelector('.button-O'),
 
-    RESULT: document.querySelector('.button-result'),
-    ADD: document.querySelector('.button-add '),
-    SUBTRACT: document.querySelector('.button-subtract'),
-    MULTI: document.querySelector('.button-multi'),
-    DIVIDE: document.querySelector('.button-divide'),
-    DELETE: document.querySelector('.button-delete'),
-    CLEAR: document.querySelector('.button-C'),
+    SIGNS: document.querySelectorAll('.button-sign'),
+    // ADD: document.querySelector('.button-add'),
+    // SUBTRACT: document.querySelector('.button-subtract'),
+    // MULTI: document.querySelector('.button-multi'),
+    // DIVIDE: document.querySelector('.button-divide'),
   },
-
+  DELETE: document.querySelector('.button-delete'),
+  CLEAR: document.querySelector('.button-C'),
   TEXT: document.querySelector('.calc__display-text'),
+  RESULT: document.querySelector('.button-result'),
 }
 console.log(UI.BUTTONS)
 
 UI.BUTTONS.NUMBERS.forEach((button) =>
   button.addEventListener('click', showButtonText)
 )
-UI.BUTTONS.CLEAR.addEventListener('click', clearResultText)
-UI.BUTTONS.DELETE.addEventListener('click', deleteLastDigit)
+UI.CLEAR.addEventListener('click', clearResultText)
+UI.DELETE.addEventListener('click', deleteLastDigit)
+UI.BUTTONS.RESULT.addEventListener('click', calculate)
 
 function showButtonText(e) {
   let buttonText = ''
   buttonText = e.target.textContent
-  if (UI.TEXT.textContent === 'O') {
+  if (UI.TEXT.textContent === '0') {
     UI.TEXT.textContent = ''
   }
   UI.TEXT.textContent += buttonText
@@ -44,4 +45,35 @@ function clearResultText() {
   UI.TEXT.textContent = '0'
 }
 
-function deleteLastDigit() {}
+function deleteLastDigit() {
+  const displayValue = UI.TEXT.textContent
+  if (displayValue.length > 1) {
+    UI.TEXT.textContent = displayValue.slice(0, -1)
+  } else {
+    UI.TEXT.textContent = '0'
+  }
+}
+
+function calculate() {
+  let num1 = ''
+  let num2 = ''
+  const operation = UI.BUTTONS.SIGNS.textContent
+  const calcResult = calc(operation, num1, num2)
+
+  if (UI.BUTTONS.SIGNS.textContent) {
+    git
+  }
+}
+
+function calc(operation, a, b) {
+  switch (operation) {
+    case 'ADD':
+      return a + b
+    case 'SUBTRACT':
+      return a - b
+    case 'MULTI':
+      return a * b
+    case 'DIVIDE':
+      return a / b
+  }
+}
