30d32edc3101c9831c516aab69a8a9a01fdd50ec
innamagkeeva <inna.m79@mail.ru>
2024-09-06 03:27:08 +0300

    calculate()неХочет

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
-let a = '' //первое число
-let b = '' //второе число
-let sign = '' //знак операции
+let firstNumber = ''
+let secondNumber = ''
+let sign = ''
 
 const UI = {
-  BUTTONS: document.querySelectorAll('.button'),
-  // TWO: document.querySelector('.button-2'),
-  // THREE: document.querySelector('.button-3'),
-  // FOUR: document.querySelector('.button-4'),
-  // FIVE: document.querySelector('.button-5'),
-  // SIX: document.querySelector('.button-6'),
-  // SEVEN: document.querySelector('.button-7'),
-  // EIGHT: document.querySelector('.button-8'),
-  // NINE: document.querySelector('.button-9'),
-  // ZERO: document.querySelector('.button-O'),
-
-  // SIGNS: document.querySelectorAll('.button-sign'),
-  // ADD: document.querySelector('.button-add'),
-  // SUBTRACT: document.querySelector('.button-subtract'),
-  // MULTI: document.querySelector('.button-multi'),
-  // DIVIDE: document.querySelector('.button-divide'),
-
+  BUTTONS: {
+    NUMBERS: document.querySelectorAll('.button-number'),
+    SIGNS: document.querySelectorAll('.button-sign'),
+  },
   DELETE: document.querySelector('.button-delete'),
   CLEAR: document.querySelector('.button-C'),
   TEXT: document.querySelector('.calc__display-text'),
@@ -27,34 +14,81 @@ const UI = {
 }
 console.log(UI.BUTTONS)
 
-UI.BUTTONS.forEach((button) => button.addEventListener('click', showButtonText))
 UI.CLEAR.addEventListener('click', clearResultText)
 UI.DELETE.addEventListener('click', deleteLastDigit)
-// UI.BUTTONS.RESULT.addEventListener('click', calculate)
+
+UI.BUTTONS.NUMBERS.forEach((button) =>
+  button.addEventListener('click', showButtonText)
+)
+UI.BUTTONS.SIGNS.forEach((button) =>
+  button.addEventListener('click', showSignText)
+)
+
+UI.RESULT.addEventListener('click', calculate)
+
+function clearResultText() {
+  UI.TEXT.textContent = '0'
+}
+
+function deleteLastDigit() {
+  const displayValue = UI.TEXT.textContent
+  if (displayValue.length > 1) {
+    UI.TEXT.textContent = displayValue.slice(0, -1)
+  } else {
+    UI.TEXT.textContent = '0'
+  }
+}
 
 function showButtonText(e) {
-  let buttonText = ''
-  buttonText = e.target.textContent
+  const buttonText = e.target.textContent
+
   if (UI.TEXT.textContent === '0') {
     UI.TEXT.textContent = ''
   }
-  if (b === '' && sign === '') {
-    a += buttonText
-    UI.TEXT.textContent = a
+  if (secondNumber === '' && sign === '') {
+    firstNumber += buttonText
+    UI.TEXT.textContent = firstNumber
     console.log(UI.TEXT.textContent)
   } else {
-    b += buttonText
-    UI.TEXT.textContent = b
+    secondNumber += buttonText
+    UI.TEXT.textContent = secondNumber
     console.log(UI.TEXT.textContent)
     return
   }
+}
 
-  if (UI.BUTTONS.textContent) {
-    sign = buttonText
+function showSignText(e) {
+  const signText = e.target.textContent
+  console.log('signText = ', signText)
+  if (signText) {
+    sign = signText
     UI.TEXT.textContent = sign
     console.log(UI.TEXT.textContent)
     return
   }
+}
+
+function calculate() {
+  const a = firstNumber
+  const b = secondNumber
+  console.log('a = ', firstNumber)
+  console.log('b = ', secondNumber)
+
+  switch (sign) {
+    case '+':
+      a = number(a) + number(b)
+      break
+    case '-':
+      a = number(a) - number(b)
+      break
+    case '*':
+      a = number(a) * number(b)
+      break
+    case '/':
+      a = number(a) / number(b)
+      break
+  }
+  UI.TEXT.textContent = a
 }
 
 // if (UI.BUTTONS.RESULT) {
@@ -75,19 +109,6 @@ function showButtonText(e) {
 // }
 // UI.TEXT.textContent = a
 // console.log(a, b, operation)
-
-function clearResultText() {
-  UI.TEXT.textContent = '0'
-}
-
-function deleteLastDigit() {
-  const displayValue = UI.TEXT.textContent
-  if (displayValue.length > 1) {
-    UI.TEXT.textContent = displayValue.slice(0, -1)
-  } else {
-    UI.TEXT.textContent = '0'
-  }
-}
 
 // function calc(operation, a, b) {
 //   switch (operation) {
